b6b3125b6dbbbe846b8a4430ac3ceafc2f4946d4
lucas tostee <22588842+luctst@users.noreply.github.com>
2023-11-21 15:32:07 +0100

    chore: init trpc server (#1)

---
+++ apps/server/index.ts
@@ -0,0 +1,33 @@
+import { createHTTPServer } from '@trpc/server/adapters/standalone';
+import { z } from 'zod';
+// import { db } from './db';
+import { publicProcedure, router } from './trpc';
+
+const appRouter = router({
+  userList: publicProcedure
+    .query(async () => {
+      // Retrieve users from a datasource, this is an imaginary database
+      // const users = await db.user.findMany();
+      // return users;
+      return 'Hello world';
+    }),
+  userById: publicProcedure
+    .input(z.string())
+    .query(async (opts) => {
+      const { input } = opts;
+      // Retrieve the user with the given ID
+      // const user = await db.user.findById(input);
+      // return user;
+      return `{ id: ${input}}`;
+    }),
+});
+ 
+// Export type router type signature,
+// NOT the router itself.
+// export type AppRouter = typeof appRouter;
+
+const server = createHTTPServer({
+  router: appRouter,
+});
+
+server.listen(3000);
