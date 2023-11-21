4095701ab0a5745d3c3e92f959b711407cca7ce4
btafforeau <93326865+btafforeau@users.noreply.github.com>
2023-11-21 17:06:23 +0100

    Adding checkout template (#5)
    Server createOrder and captureOrder (#2)
    
    Co-authored-by: Matthias RAIGNE <5262628+Matt75@users.noreply.github.com>

--- apps/client/src/App.vue
+++ apps/client/src/App.vue
@@ -1,17 +1,30 @@
-<script setup lang="ts">
-import trpc from './trpc';
-import { onMounted, ref } from 'vue';
+<script>
+import CheckoutView from "./views/CheckoutView.vue";
+import { loadScript } from "@paypal/paypal-js";
 
-const data = ref<unknown>();
+const CLIENT_ID = 'AcG9tUFUS4-MEUvw0QU1Qeicds-pn0GJy27nLHwbrGJPtX1GSRjMxLPKrqY_Oo5VfshkHr2Tdx7ZNEpn';
+const MERCHANT_ID = 'WGB2BUWE22BZE';
+const PARTNER_ATTRIBUTION_ID = 'PrestaShop_Cart_PSXO_Testing';
 
-onMounted(async () => {
-  data.value = await trpc.userList.query();
-});
+let paypal;
+
+try {
+  paypal = await loadScript({
+    clientId: CLIENT_ID,
+    merchantId: MERCHANT_ID,
+    dataPartnerAttributionId: PARTNER_ATTRIBUTION_ID,
+  });
+} catch (error) {
+  console.error("failed to load the PayPal JS SDK script", error);
+}
+
+export default {
+  components: {CheckoutView}
+}
 </script>
 
 <template>
-  <h1>{{ data }}</h1>
+  <main>
+    <CheckoutView />
+  </main>
 </template>
-
-<style scoped>
-</style>
---
+++ apps/client/src/components/PayPalCheckoutButton.vue
@@ -0,0 +1,9 @@
+<script>
+export default {
+  name: "PayPalCheckoutButton"
+}
+</script>
+
+<template>
+  <div id="paypal-checkout"></div>
+</template>
