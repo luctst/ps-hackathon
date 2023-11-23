2e7bcd3277062d9ca7ea5df95385b1d48fee1b3b
Matt75 <5262628+Matt75@users.noreply.github.com>
2023-11-23 12:12:24 +0100

    Add card-fields

--- apps/client/src/App.vue
+++ apps/client/src/App.vue
@@ -1,14 +1,14 @@
 <script setup lang="ts">
 import { onMounted, ref } from 'vue';
 import CheckoutView from "./views/CheckoutView.vue";
-import { loadScript } from "@paypal/paypal-js";
+import {loadScript, PayPalNamespace} from "@paypal/paypal-js";
 
 const CLIENT_ID = 'AcG9tUFUS4-MEUvw0QU1Qeicds-pn0GJy27nLHwbrGJPtX1GSRjMxLPKrqY_Oo5VfshkHr2Tdx7ZNEpn';
 const MERCHANT_ID = 'WGB2BUWE22BZE';
 const PARTNER_ATTRIBUTION_ID = 'PrestaShop_Cart_PSXO_Testing';
 
 
-const paypal = ref<null>();
+const paypal = ref<PayPalNamespace | null>();
 const isSdkLoaded = ref<boolean>(false);
 
 onMounted(async () => {
@@ -17,6 +17,7 @@ onMounted(async () => {
       clientId: CLIENT_ID,
       merchantId: MERCHANT_ID,
       dataPartnerAttributionId: PARTNER_ATTRIBUTION_ID,
+      components: ['buttons', 'card-fields'],
     });
     isSdkLoaded.value = true;
   } catch (error) {
